b81f96cdaa36fb535fbc6bf3f382d04ec6cca8c4
Kenner10 <142851488+Kenner10@users.noreply.github.com>
2025-01-22 03:42:01 +0000

    Tarea 1-Prueba

---
+++ Practica1.py
@@ -0,0 +1,24 @@
+#1
+print("hola mundo desde github")
+valor= input("ingrese datos")
+#5
+nombre = input("Ingresa tu nombre: ")
+print("Hola,", nombre)
+#6
+edad= int(input("ingresa tu edad: "))
+if edad > 18:
+    print("Mayor de edad.")
+else:
+    print("Menor de edad.")
+
+#7
+Numero= int( input("ingresa numero: "))
+if Numero % 2 == 0 :
+    print("Par.")
+else:
+    print("impar.")
+
+#8
+Numero_1= int(input("ingrese numero entero:"))
+suma= sum(range(0,Numero_1 +1))
+print("la suma de todos los numeros.", Numero_1, "es.", suma )
